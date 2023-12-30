7c30a0295017f26a5805635d86e56bed9d8ab964
Chris Cranford <chris@hibernate.org>
2023-12-30 04:26:48 -0500

    GH-43 More cmake fixes

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -17,30 +17,30 @@
 
 find_program(CLANG_FORMAT_PROGRAM NAMES clang-format)
 
-if (CLANG_FORMAT_PROGRAM)
-    execute_process(
-            COMMAND "${CLANG_FORMAT_PROGRAM}" --version
-            OUTPUT_VARIABLE CLANG_FORMAT_VERSION
-            OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-
-    message("Using clang-format: ${CLANG_FORMAT_PROGRAM} (${CLANG_FORMAT_VERSION})")
-
-    file(GLOB_RECURSE
-            format_src_list
-            RELATIVE
-            "${CMAKE_CURRENT_SOURCE_DIR}"
-            "src/*.[hc]"
-            "src/*.[hc]pp"
-    )
-
-    foreach (_src_file ${format_src_list})
-        message("    formatting => ${_src_file}")
-        execute_process(
-                COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
-                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-        )
-    endforeach ()
-
-    unset(CLANG_FORMAT_VERSION)
-endif ()
+#if (CLANG_FORMAT_PROGRAM)
+#    execute_process(
+#            COMMAND "${CLANG_FORMAT_PROGRAM}" --version
+#            OUTPUT_VARIABLE CLANG_FORMAT_VERSION
+#            OUTPUT_STRIP_TRAILING_WHITESPACE
+#    )
+#
+#    message("Using clang-format: ${CLANG_FORMAT_PROGRAM} (${CLANG_FORMAT_VERSION})")
+#
+#    file(GLOB_RECURSE
+#            format_src_list
+#            RELATIVE
+#            "${CMAKE_CURRENT_SOURCE_DIR}"
+#            "src/*.[hc]"
+#            "src/*.[hc]pp"
+#    )
+#
+#    foreach (_src_file ${format_src_list})
+#        message("    formatting => ${_src_file}")
+#        execute_process(
+#                COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
+#                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+#        )
+#    endforeach ()
+#
+#    unset(CLANG_FORMAT_VERSION)
+#endif ()
