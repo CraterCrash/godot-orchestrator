182e16d515781f56648a1f5bfcd64e71184e653f
Chris Cranford <chris@hibernate.org>
2024-04-25 00:24:27 -0400

    Fix comparison signed/unsigned mismatch

--- src/script/nodes/signals/emit_member_signal.cpp
+++ src/script/nodes/signals/emit_member_signal.cpp
@@ -45,7 +45,7 @@ public:
 
             if (!_method.arguments.empty())
             {
-                if (_args.size() != _method.arguments.size())
+                if (uint64_t(_args.size()) != _method.arguments.size())
                     _args.resize(_method.arguments.size());
 
                 for (int i = 0; i < _method.arguments.size(); i++)
