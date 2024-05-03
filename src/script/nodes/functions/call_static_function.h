55ab273d24b92f7a368875c8294781a83e211133
Chris Cranford <chris@hibernate.org>
2024-05-02 23:30:10 -0400

    GH-284 Add support for static functions

---
+++ src/script/nodes/functions/call_static_function.h
@@ -0,0 +1,55 @@
+// This file is part of the Godot Orchestrator project.
+//
+// Copyright (c) 2023-present Vahera Studios LLC and its contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef ORCHESTRATOR_SCRIPT_NODE_CALL_STATIC_FUNCTION_H
+#define ORCHESTRATOR_SCRIPT_NODE_CALL_STATIC_FUNCTION_H
+
+#include "script/node.h"
+
+class OScriptNodeCallStaticFunction : public OScriptNode
+{
+    ORCHESTRATOR_NODE_CLASS(OScriptNodeCallStaticFunction, OScriptNode);
+    static void _bind_methods() { }
+
+protected:
+    StringName _class_name;
+    StringName _method_name;
+    MethodInfo _method;
+    GDExtensionMethodBindPtr _method_bind{ nullptr };
+
+    //~ Begin Wrapped Interface
+    void _get_property_list(List<PropertyInfo>* r_list) const;
+    bool _get(const StringName& p_name, Variant& r_value) const;
+    bool _set(const StringName& p_name, const Variant& p_value);
+    //~ End Wrapped Interface
+
+    void _resolve_method_info();
+
+public:
+    //~ Begin OScriptNode Interface
+    void post_initialize() override;
+    void post_placed_new_node() override;
+    void allocate_default_pins() override;
+    String get_tooltip_text() const override;
+    String get_node_title() const override;
+    String get_node_title_color_name() const override { return "function_call"; }
+    bool validate_node_during_build() const override;
+    OScriptNodeInstance* instantiate(OScriptInstance* p_instance) override;
+    void initialize(const OScriptNodeInitContext& p_context) override;
+    //~ End OScriptNode Interface
+};
+
+#endif // ORCHESTRATOR_SCRIPT_NODE_CALL_STATIC_FUNCTION_H
