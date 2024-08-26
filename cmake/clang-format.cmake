30eea75518767f382824df6f77a113f9e7c32a99
Chris Cranford <chris@hibernate.org>
2024-08-25 23:41:14 -0400

    Build system cleanup and refactor

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -14,33 +14,31 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 ##
+INCLUDE_GUARD()
 
 find_program(CLANG_FORMAT_PROGRAM NAMES clang-format)
+IF (CLANG_FORMAT_PROGRAM)
+    EXECUTE_PROCESS(
+            COMMAND "${CLANG_FORMAT_PROGRAM}" --version
+            OUTPUT_VARIABLE CLANG_FORMAT_VERSION
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-#if (CLANG_FORMAT_PROGRAM)
-#    execute_process(
-#            COMMAND "${CLANG_FORMAT_PROGRAM}" --version
-#            OUTPUT_VARIABLE CLANG_FORMAT_VERSION
-#            OUTPUT_STRIP_TRAILING_WHITESPACE
-#    )
-#
-#    message("Using clang-format: ${CLANG_FORMAT_PROGRAM} (${CLANG_FORMAT_VERSION})")
-#
-#    file(GLOB_RECURSE
-#            format_src_list
-#            RELATIVE
-#            "${CMAKE_CURRENT_SOURCE_DIR}"
-#            "src/*.[hc]"
-#            "src/*.[hc]pp"
-#    )
-#
-#    foreach (_src_file ${format_src_list})
-#        message("    formatting => ${_src_file}")
-#        execute_process(
-#                COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
-#                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-#        )
-#    endforeach ()
-#
-#    unset(CLANG_FORMAT_VERSION)
-#endif ()
+    MESSAGE("Using clang-format: ${CLANG_FORMAT_PROGRAM} (${CLANG_FORMAT_VERSION})")
+
+    FILE(GLOB_RECURSE
+            format_src_list
+            RELATIVE
+            "${CMAKE_CURRENT_SOURCE_DIR}"
+            "src/*.[hc]"
+            "src/*.[hc]pp"
+    )
+
+    FOREACH (_src_file ${format_src_list})
+        MESSAGE("    formatting => ${_src_file}")
+        EXECUTE_PROCESS(
+                COMMAND "${CLANG_FORMAT_PROGRAM}" --style=file -i "${_src_file}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+    ENDFOREACH ()
+
+    UNSET(CLANG_FORMAT_VERSION)
+ENDIF ()
