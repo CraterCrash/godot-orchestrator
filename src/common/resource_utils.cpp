8db0fa3ddd34561695fe3a63f6fbe76c68ddc910
Chris Cranford <chris@hibernate.org>
2025-07-05 09:36:13 -0400

    GH-974 Support reloading script from disk

---
+++ src/common/resource_utils.cpp
@@ -0,0 +1,25 @@
+// This file is part of the Godot Orchestrator project.
+//
+// Copyright (c) 2023-present Crater Crash Studios LLC and its contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//		http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include "common/resource_utils.h"
+
+namespace ResourceUtils
+{
+    bool is_file(const String& p_path)
+    {
+        return p_path.begins_with("res://") && p_path.find("::") == -1;
+    }
+}
