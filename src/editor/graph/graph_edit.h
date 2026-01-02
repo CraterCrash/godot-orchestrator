b96b63c680a3cc85dfbc4887f226c73cffd0f7e5
Chris Cranford <chris@hibernate.org>
2026-01-02 03:32:06 -0500

    GH-1151 Add ui/nodes/connection_hotzone_scale option

--- src/editor/graph/graph_edit.h
+++ src/editor/graph/graph_edit.h
@@ -172,6 +172,8 @@ protected:
     void _drop_data_function(const Dictionary& p_function, const Vector2& p_at_position, bool p_as_callable);
     void _drop_data_variable(const String& p_name, const Vector2& p_at_position, bool p_validated, bool p_setter);
 
+    bool _is_in_port_hotzone(const Vector2& p_pos, const Vector2& p_mouse_pos, const Vector2i& p_port_size, bool p_left);
+
 public:
     // The OrchestratorGraphEdit maintains a static clipboard so that data can be shared across different graph
     // instances easily in the tab view, and so these methods are called by the MainView during the
@@ -257,6 +259,8 @@ public:
     void _drop_data(const Vector2& p_position, const Variant& p_data) override;
     bool _is_node_hover_valid(const StringName& p_from, int p_from_port, const StringName& p_to, int p_to_port) override;
     PackedVector2Array _get_connection_line(const Vector2& p_from_position, const Vector2& p_to_position) const override;
+    bool _is_in_input_hotzone(Object* p_in_node, int32_t p_in_port, const Vector2& p_mouse_position) override;
+    bool _is_in_output_hotzone(Object* p_in_node, int32_t p_in_port, const Vector2& p_mouse_position) override;
     //~ End GraphEdit overrides
 
     /// Spawn a node in the graph
