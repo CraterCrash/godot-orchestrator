c39d00a7b859939503fde27d63d3dfe9e2ecbbbf
Chris Cranford <chris@hibernate.org>
2026-01-11 09:52:27 -0500

    GH-967 Fix node path default value rendering

--- src/editor/graph/pins/node_path_pin.cpp
+++ src/editor/graph/pins/node_path_pin.cpp
@@ -17,16 +17,132 @@
 #include "editor/graph/pins/node_path_pin.h"
 
 #include "common/macros.h"
+#include "common/string_utils.h"
 #include "editor/dialogs_helper.h"
+#include "editor/graph/graph_node.h"
 #include "editor/property_selector.h"
 #include "editor/scene_node_selector.h"
 #include "script/nodes/functions/call_member_function.h"
+#include "script/nodes/properties/property_set.h"
 #include "script/nodes/utilities/self.h"
 
 #include <godot_cpp/classes/editor_interface.hpp>
 #include <godot_cpp/classes/scene_tree.hpp>
 
-Vector<OrchestratorEditorGraphPinNodePath::MethodDescriptor> OrchestratorEditorGraphPinNodePath::_descriptors;
+Vector<OrchestratorEditorGraphPinNodePath::DependencyDescriptor> OrchestratorEditorGraphPinNodePath::_descriptors;
+
+OrchestratorEditorGraphPinNodePath::DependencyDescriptor* OrchestratorEditorGraphPinNodePath::_resolve_descriptor()
+{
+    // Note: The parent type's pin is not yet set, so don't use any parent methods here
+    ERR_FAIL_COND_V(!_owning_pin.is_valid(), nullptr);
+    OScriptNode* owning_node = _owning_pin->get_owning_node();
+
+    if (OScriptNodeCallMemberFunction* cmf = cast_to<OScriptNodeCallMemberFunction>(owning_node))
+    {
+        const String target_class = cmf->get_target_class();
+        const MethodInfo method = cmf->get_function();
+
+        for (DependencyDescriptor& descriptor : _descriptors)
+        {
+            if (!target_class.match(descriptor.class_name))
+                continue;
+
+            if (!method.name.match(descriptor.method_name))
+                continue;
+
+            if (!_owning_pin->get_pin_name().match(descriptor.method_argument_name))
+                continue;
+
+            return &descriptor;
+        }
+    }
+    else if (OScriptNodePropertySet* ps = cast_to<OScriptNodePropertySet>(owning_node))
+    {
+        if (ps->get_call_mode() == OScriptNodePropertySet::CALL_INSTANCE)
+        {
+            for (DependencyDescriptor& descriptor : _descriptors)
+            {
+                if (!ps->get_base_type().match(descriptor.class_name))
+                    continue;
+
+                if (!ps->get_property().name.match(descriptor.property_name))
+                    continue;
+
+                return &descriptor;
+            }
+        }
+    }
+
+    return nullptr;
+}
+
+void OrchestratorEditorGraphPinNodePath::_configure_descriptor(DependencyDescriptor* p_descriptor)
+{
+    _descriptor = p_descriptor;
+
+    if (_descriptor)
+    {
+        OScriptNode* owning_node = _owning_pin->get_owning_node();
+        const Ref<OScriptNodePin> dep_pin = owning_node->find_pin(_descriptor->dependency_pin_name, PD_Input);
+        if (dep_pin.is_valid() && !dep_pin->has_any_connections())
+            _set_button_state(true);
+
+        owning_node->connect("pin_connected", callable_mp_this(_pin_connected));
+        owning_node->connect("pin_disconnected", callable_mp_this(_pin_disconnected));
+    }
+}
+
+bool OrchestratorEditorGraphPinNodePath::_is_only_node_selection_required() const
+{
+    return !_descriptor || (_descriptor && !_descriptor->is_node_and_property_selection); // NOLINT
+}
+
+Ref<OrchestrationGraphPin> OrchestratorEditorGraphPinNodePath::_get_dependency_object_pin()
+{
+    if (!_descriptor || _owning_pin.is_null())
+        return {};
+
+    return _owning_pin->get_owning_node()->find_pin(_descriptor->dependency_pin_name, PD_Input);
+}
+
+void OrchestratorEditorGraphPinNodePath::_set_button_state(bool p_disabled, bool p_reset)
+{
+    _set_button_disabled(p_disabled);
+
+    const String tooltip = p_disabled
+        ? vformat("Connect the %s pin to pick a %s",
+            _descriptor->dependency_pin_name,
+            StringUtils::default_if_empty(_descriptor->method_argument_name, _descriptor->property_name))
+        : "";
+    _set_button_tooltip(tooltip);
+
+    if (p_reset)
+        _set_default_value(Variant());
+}
+
+void OrchestratorEditorGraphPinNodePath::_pin_connected(int p_type, int p_index)
+{
+    ERR_FAIL_COND(!_owning_pin.is_valid());
+
+    OScriptNode* node = _owning_pin->get_owning_node();
+    ERR_FAIL_NULL(node);
+
+    const Ref<OScriptNodePin> pin = node->find_pin(p_index, static_cast<EPinDirection>(p_type));
+    if (_descriptor && pin.is_valid() && pin->get_pin_name().match(_descriptor->dependency_pin_name))
+        _set_button_state(false);
+}
+
+void OrchestratorEditorGraphPinNodePath::_pin_disconnected(int p_type, int p_index)
+{
+    ERR_FAIL_COND(!_owning_pin.is_valid());
+
+    OScriptNode* node = _owning_pin->get_owning_node();
+    ERR_FAIL_NULL(node);
+
+    const Ref<OScriptNodePin> pin = node->find_pin(p_index, static_cast<EPinDirection>(p_type));
+    if (_descriptor && pin.is_valid() && pin->get_pin_name().match(_descriptor->dependency_pin_name))
+        _set_button_state(true, true);
+}
 
 void OrchestratorEditorGraphPinNodePath::_open_node_selector()
 {
@@ -59,9 +175,8 @@ void OrchestratorEditorGraphPinNodePath::_node_selected(const NodePath& p_path)
         return;
     }
 
-    if (!_descriptor || (_descriptor && !_descriptor->is_node_and_property_selection))
+    if (_is_only_node_selection_required())
     {
-        // Only a node selection is required
         _handle_selector_button_response(p_path);
         return;
     }
@@ -87,7 +202,7 @@ void OrchestratorEditorGraphPinNodePath::_open_property_selector(Object* p_objec
 
     if (p_object == nullptr)
     {
-        const Ref<OrchestrationGraphPin> pin = _owning_pin->get_owning_node()->find_pin(_descriptor->dependency_pin_name, PD_Input);
+        const Ref<OrchestrationGraphPin> pin = _get_dependency_object_pin();
         if (!pin.is_valid() || !pin->has_any_connections())
             return;
 
@@ -140,44 +255,26 @@ void OrchestratorEditorGraphPinNodePath::_handle_selector_button_pressed()
         _open_property_selector();
 }
 
-void OrchestratorEditorGraphPinNodePath::set_owning_pin(const Ref<OrchestrationGraphPin>& p_pin)
+void OrchestratorEditorGraphPinNodePath::set_pin(const Ref<OrchestrationGraphPin>& p_pin)
 {
     _owning_pin = p_pin;
 
-    const Ref<OScriptNodeCallMemberFunction> cmf = cast_to<OScriptNodeCallMemberFunction>(p_pin->get_owning_node());
-    if (cmf.is_valid())
-    {
-        const String target_class = cmf->get_target_class();
-        const MethodInfo method = cmf->get_function();
+    // This needs to be resolved before setting parent pin type
+    _descriptor = _resolve_descriptor();
+    parent_type::set_pin(p_pin);
 
-        for (MethodDescriptor& descriptor : _descriptors)
-        {
-            if (!target_class.match(descriptor.class_name))
-                continue;
-
-            if (!method.name.match(descriptor.method_name))
-                continue;
-
-            if (!_owning_pin->get_pin_name().match(descriptor.pin_name))
-                continue;
-
-            _descriptor = &descriptor;
-            break;
-        }
-    }
-
-    if (_descriptor)
-    {
-        const Ref<OrchestrationGraphPin> target = _owning_pin->get_owning_node()->find_pin(_descriptor->dependency_pin_name, PD_Input);
-        if (target.is_valid() && !target->has_any_connections())
-            _set_button_visible(false);
-    }
+    // Now configure the descriptor behavior
+    _configure_descriptor(_descriptor);
 }
 
 void OrchestratorEditorGraphPinNodePath::_bind_methods()
 {
-    _descriptors.push_back({ "Tween", "tween_property", "property", "object", true, false });
-    _descriptors.push_back({ "AnimationMixer", "set_root_motion_track", "path", "", false, true, true });
+    // Method Overrides
+    _descriptors.push_back({ "Tween", "tween_property", "property", "", "object", true, false });
+    _descriptors.push_back({ "AnimationMixer", "set_root_motion_track", "path", "", "", false, true, true });
+
+    // Property Overrides
+    _descriptors.push_back({ "AnimationMixer", "", "", "root_motion_track", "", false, true, true });
 }
 
 OrchestratorEditorGraphPinNodePath::OrchestratorEditorGraphPinNodePath()
