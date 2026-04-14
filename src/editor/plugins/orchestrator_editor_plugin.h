47a6b53b1b4f2ec81b01318d66ccff4922317bc7
Chris Cranford <chris@hibernate.org>
2026-04-14 00:53:47 -0400

    GH-1515 Cleanup editor plugins on exit

--- src/editor/plugins/orchestrator_editor_plugin.h
+++ src/editor/plugins/orchestrator_editor_plugin.h
@@ -19,6 +19,7 @@
 #include "editor/debugger/script_debugger_plugin.h"
 
 #include <godot_cpp/classes/config_file.hpp>
+#include <godot_cpp/classes/editor_export_plugin.hpp>
 #include <godot_cpp/classes/editor_inspector_plugin.hpp>
 #include <godot_cpp/classes/editor_interface.hpp>
 #include <godot_cpp/classes/editor_plugin.hpp>
@@ -39,7 +40,38 @@ class OrchestratorPlugin : public EditorPlugin {
     String _last_editor;                                      //! Last editor
     OrchestratorEditor* _editor_panel = nullptr;              //! Plugin's editor panel
     OrchestratorWindowWrapper* _window_wrapper = nullptr;     //! Window wrapper
+    Vector<Ref<EditorExportPlugin>> _export_plugins;
     Vector<Ref<EditorInspectorPlugin>> _inspector_plugins;
+    Vector<Ref<EditorDebuggerPlugin>> _debugger_plugins;
+
+    template <typename T>
+    struct PluginTraits;
+
+    template <typename T>
+    using PluginBase =
+        std::conditional_t<std::is_base_of_v<EditorExportPlugin, T>, EditorExportPlugin,
+        std::conditional_t<std::is_base_of_v<EditorInspectorPlugin, T>, EditorInspectorPlugin,
+        std::conditional_t<std::is_base_of_v<EditorDebuggerPlugin, T>, EditorDebuggerPlugin,
+        void>>>;
+
+    template <typename T>
+    void _register_plugin() {
+        static_assert(!std::is_same_v<PluginBase<T>, void>, "T is not recognized");
+
+        Ref<T> plugin;
+        plugin.instantiate();
+        PluginTraits<PluginBase<T>>::storage(this).push_back(plugin);
+        PluginTraits<PluginBase<T>>::add(this, plugin);
+    }
+
+    template <typename Base>
+    void _unregister_plugin() {
+        for (const Ref<Base>& plugin : PluginTraits<Base>::storage(this)) {
+            PluginTraits<Base>::remove(this, plugin);
+            plugin->unreference();
+        }
+        PluginTraits<Base>::storage(this).clear();
+    }
 
     static String _get_orchestrator_metedata_path();
 
@@ -47,32 +79,11 @@ class OrchestratorPlugin : public EditorPlugin {
     bool _is_exiting() const;
 
     void _register_plugins();
+    void _unregister_plugins();
 
     bool _is_plugin_just_installed() const;
 
     void _add_plugin_icon_to_editor_theme();
-
-    template <typename T>
-    void _register_inspector_plugin() {
-        Ref<T> plugin;
-        plugin.instantiate();
-        _inspector_plugins.push_back(plugin);
-        add_inspector_plugin(plugin);
-    }
-
-    template <typename T>
-    void _register_export_plugin() {
-        Ref<T> plugin;
-        plugin.instantiate();
-        add_export_plugin(plugin);
-    }
-
-    template <typename T>
-    void _register_debugger_plugin() {
-        Ref<T> plugin;
-        plugin.instantiate();
-        add_debugger_plugin(plugin);
-    }
 
     //~ Begin Signals
     void _main_screen_changed(const String& p_name);
@@ -124,8 +135,9 @@ public:
     void make_active();
 
     template<typename T>
-    Ref<T> get_editor_inspector_plugin() {
-        for (Ref<EditorInspectorPlugin>& plugin : _inspector_plugins) {
+    Ref<T> get_plugin() {
+        static_assert(!std::is_same_v<PluginBase<T>, void>, "T is not recognized");
+        for (Ref<PluginBase<T>>& plugin : PluginTraits<PluginBase<T>>::storage(this)) {
             if (T* result = cast_to<T>(plugin.ptr())) {
                 return result;
             }
@@ -134,4 +146,25 @@ public:
     }
 
     OrchestratorPlugin();
+};
+
+template <>
+struct OrchestratorPlugin::PluginTraits<EditorInspectorPlugin> {
+    static auto& storage(OrchestratorPlugin* self) { return self->_inspector_plugins; }
+    static void add(OrchestratorPlugin* self, Ref<EditorInspectorPlugin> p) { self->add_inspector_plugin(p); }
+    static void remove(OrchestratorPlugin* self, Ref<EditorInspectorPlugin> p) { self->remove_inspector_plugin(p); }
+};
+
+template <>
+struct OrchestratorPlugin::PluginTraits<EditorExportPlugin> {
+    static auto& storage(OrchestratorPlugin* self) { return self->_export_plugins; }
+    static void add(OrchestratorPlugin* self, Ref<EditorExportPlugin> p) { self->add_export_plugin(p); }
+    static void remove(OrchestratorPlugin* self, Ref<EditorExportPlugin> p) { self->remove_export_plugin(p); }
+};
+
+template <>
+struct OrchestratorPlugin::PluginTraits<EditorDebuggerPlugin> {
+    static auto& storage(OrchestratorPlugin* self) { return self->_debugger_plugins; }
+    static void add(OrchestratorPlugin* self, Ref<EditorDebuggerPlugin> p) { self->add_debugger_plugin(p); }
+    static void remove(OrchestratorPlugin* self, Ref<EditorDebuggerPlugin> p) { self->remove_debugger_plugin(p); }
 };
